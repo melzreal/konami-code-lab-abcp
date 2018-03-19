e730cb98b19e2734ef6e98f7a2817abe5aa81cb1
Melissa Real <melissa.real@hotmail.com>
2018-03-19 17:30:29 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -5,11 +5,8 @@ let pressed = 0;
 function init() {
   
   document.body.addEventListener('keydown', function(e){
-  
-   const key = parseInt(e.detail || e.which);
-   console.log(e.detail, e.which);
-   console.log(key);
-  
+  const key = parseInt(e.detail || e.which);
+
   if (key === code[pressed]) {
     pressed++;
  
