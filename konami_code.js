75bc06b2ed07a6e07e52c721bef39736dc49f1ac
Melissa Real <melissa.real@hotmail.com>
2018-03-19 17:29:52 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,27 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+//up up down down left right left right b a
+let pressed = 0;
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  
+  document.body.addEventListener('keydown', function(e){
+  
+   const key = parseInt(e.detail || e.which);
+   console.log(e.detail, e.which);
+   console.log(key);
+  
+  if (key === code[pressed]) {
+    pressed++;
+ 
+    if (pressed === code.length) {
+      alert("Holeee!");
+ 
+      index = 0;
+    }
+  } else {
+   
+    index = 0;
+  }
+});
 }
+
